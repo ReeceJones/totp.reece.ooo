bed297b7382e72b2cb9e25b559171d72b0336353
ReeceJones <reece_jones@icloud.com>
2026-02-04 20:03:41 -0500

    update da config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = { preprocess: vitePreprocess(), kit: { adapter: adapter() } };
 
 export default config;
